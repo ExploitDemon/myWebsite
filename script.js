4ace23a1a1f7b0938f2b98921a4cc33913774447
ExploitDemon <exploitdemon@gmail.com>
2023-09-12 18:29:25 -0700

    more changes

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
-// script.js
-import Vue from 'vue';
-
-const app = new Vue({
-    el: '#app',
-    data() {
-        return {
-            content: '<span class="cole">Cole</span><span class="leavitt"> Leavitt</span>'
-        };
-    }
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+//
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+// });
