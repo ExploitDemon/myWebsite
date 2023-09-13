17ca496e30da02d851f2a85a9759743d7a7c7fcf
ExploitDemon <exploitdemon@gmail.com>
2023-09-12 17:52:27 -0700

    did some shit

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// script.js
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            content: '<span class="cole">Cole</span><span class="leavitt"> Leavitt</span>'
+        };
+    }
+});
